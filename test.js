9b4eeefbe0e37d6cd6d6baff01f31cb0834b05c1
Marvin <18121196@CNSUNING.COM>
2019-03-20 12:44:58 +0800

    改写的测试文件

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
-const file_list = require('./index');
+const { list } = require('./index');
 
 const start = Date.now();
 
-const r = file_list('/Users/mk/git/MINIP', {
+const r = list('./', {
     filters: [
-        // '*.css',
+        '*.js',
     ],
     ignores: [
-        // /sn-wxminip-components/
     ],
     ignoreHidden: false
 }).sort((a,b)=>a.localeCompare(b));
 
 r.forEach(rr => console.log(rr))
 console.log(`${r.length} files ${Date.now() - start}ms`)
-// console.log(Date.now() - start)
